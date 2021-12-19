807c5c6f917cf5532e85631a851f82a046d385ac
mustuner1972 <mustafaustuner1972@gmail.com>
2021-12-19 19:56:02 +0000

    New github bramch commit

--- src/test/java/gitHbMergeFeature/Test01.java
+++ src/test/java/gitHbMergeFeature/Test01.java
@@ -3,5 +3,6 @@ package gitHbMergeFeature;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("I made these changes locally, but I will merge in GitHub");
+        System.out.println("dsaddasda");
     }
 }
