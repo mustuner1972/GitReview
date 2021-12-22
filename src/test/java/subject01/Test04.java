a762dc93c7c7795a0722f7c4d7eb18b7f14967a3
mustuner1972 <mustafaustuner1972@gmail.com>
2021-12-22 14:14:47 +0000

    last commit

--- src/test/java/subject01/Test04.java
+++ src/test/java/subject01/Test04.java
@@ -2,6 +2,8 @@ package subject01;
 
 public class Test04 {
     public static void main(String[] args) {
-        System.out.println("4th try");
+        System.out.println("4th trial");
+        int x=2;
+
     }
 }
